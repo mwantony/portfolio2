8672a533d1a836ff85d578b2853e161f85aff9e5
Antony Maziero Warken <againplayi7@gmail.com>
2023-04-01 22:28:02 -0300

    inicio feito

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,11 +1,12 @@
 import {ReactComponent as MenuIcon} from "../../assets/svg/menu-icon.svg"
-import {ReactComponent as DarkTheme} from '../../assets/svg/lua.svg'
+import MenuAparecer from 'assets/img/menu.png'
 import styles from './Cabecalho.module.scss'
 export function Cabecalho() {
     return (
         <>
             <header className={styles.cabecalho}>
                 <MenuIcon className={styles["cabecalho__menuicon"]}></MenuIcon>
+
             </header>
         </>
     )
